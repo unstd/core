6322ba571e1c9b074654f2faeed37bd57bf64efc
unstd <nailcui@163.com>
2021-06-20 15:08:59 +0800

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/unstd/core
+
+go 1.16
---
+++ string.go
@@ -0,0 +1,9 @@
+package core
+
+func IsEmpty(str string) bool {
+	return len(str) == 0
+}
+
+func IsNotEmpty(str string) bool {
+	return len(str) > 0
+}
